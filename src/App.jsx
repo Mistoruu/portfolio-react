1a8d79aa4a5b2815746f1ac24cd6d85fe484944a
Mistoruu <maille.eliott@gmail.com>
2025-12-02 10:56:33 +0100

    fix: projet route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./styles/App.css";
 import Home from "./pages/home";
+import Project1 from "./pages/Project1";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/p1" element={<Project1 />} />
                     <Route path="*" element={<Home />} />
                 </Routes>
             </BrowserRouter>
